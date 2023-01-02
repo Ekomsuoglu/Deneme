92a923b32cb291e782b8054523184b62f74f268d
Emirhan KOMŞUOĞLU <emirhan.komsuoglu@baogrenci.com>
2023-01-02 10:35:22 +0300

    first commit

---
+++ CA_GitDeneme/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CA_GitDeneme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Canım!");
+        }
+    }
+}
